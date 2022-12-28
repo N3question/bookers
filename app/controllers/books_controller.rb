6a92c416476c13f8831e6a2b88bf58befdefcaa7
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2022-12-28 23:25:55 +0000

    [add]bookの新規登録機能の作成まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,19 @@
 class BooksController < ApplicationController
   def new
+   # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+   @book = Book.new
   end
 
   def index
+   # いらない？
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def destroy
   end
 end
