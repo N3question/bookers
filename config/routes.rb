2b0e1111928417edc1bc604dce6720c69bbce294
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2022-12-29 15:06:59 +0000

    [add]topページ・indexページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+ root 'homes#top'
+ 
  get 'books/new'
  post 'books'=>'books#create'
- get 'books/index'
- get 'books/show'
- get 'books/edit'
+ get 'books'=>'books#index'
+ get 'books/:id'=>'books#show', as:'book' #'books/:id'というリンクにアクセスしたら'books#show'を表示しますよってこと？名前付きパスはbook
+ get 'books/:id/edit'=>'books#edit', as: 'edit_book'
+ patch 'books/:id'=>'books#update' , as: 'update_book'
+ delete 'books/:id'=>'books#destroy', as: 'destroy_book'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  get 'top'=>'homes#top'
 end
